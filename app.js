c827c2e1ac95eaced6cb22985639585ed8b87d42
Tubaleviao <alvaro.prata.filho@gmail.com>
2020-11-22 19:48:40 +0000

    added certificate, environment variables and fs

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
+require('dotenv').config()
 const express = require('express'),
-  http = require('http'),
+  protocol = require('https'),
   bodyParser = require('body-parser'),
   app = express(),
-  session = require('express-session')
+  session = require('express-session'),
+  fs = require('fs')
 
-const server = http.createServer(app),
+const cert = {
+  key: fs.readFileSync(`${process.env.CERT_PATH}/privkey.pem`), 
+  cert: fs.readFileSync(`${process.env.CERT_PATH}/fullchain.pem`),
+  allowHTTP1: true
+}
+
+const server = protocol.createServer(cert, app),
   io = require('socket.io')(server),
   session_secret = process.env.SESSION_SECRET || 'nosecrethereisusedsoyeah'
-
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(__dirname + '/public'))
@@ -20,6 +27,4 @@ app.get('/home', function(req, res){res.render('home')})
 app.get('/thunder', function(req, res){res.render('thunder')})
 app.get('*', function(req, res){res.send('Nothing here.')})
 
-//io.of('/bitcoin').on('connection', code.bitcoin)
-
-server.listen(3000, function(){console.log("Server running at the port %d", 3000)})
+server.listen(3012, function(){console.log("Server running at the port %d", 3012)})
